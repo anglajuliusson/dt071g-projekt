6c4d31e1a29484ec1fe52d83259fa8065e53d187
Ängla Juliusson <angla02@icloud.com>
2025-11-03 11:20:38 +0100

    Program-klass med meny för att starta spel, visa tidigare resultat samt avsluta.

--- dt071g-projekt/Program.cs
+++ dt071g-projekt/Program.cs
@@ -5,5 +5,43 @@ using System.Text.Json;
 
 namespace rockPaperScissorsGame {
 
+    class Program {
+        public static void Main(string[] args)
+    {
+        Game game = new Game(); // Skapa spel-objekt
+        game.LoadResult(); // Läs in tidigare resultat vid start
+        bool running = true;
 
+        while (running) 
+        {
+        Console.WriteLine("=== STEN SAX PÅSE ===");
+        Console.WriteLine("Välj ett alternativ: ");
+        Console.WriteLine("1. Starta spel");
+        Console.WriteLine("2. Visa tidigare resultat");
+        Console.WriteLine("3. Avsluta");
+        
+        var choice = Console.ReadLine(); // Val av alternativ
+
+        // Alternativ 1 - Starta spel
+        if (choice == "1") {
+            game.PlayRound();
+        }
+
+        // Alternativ 2 - Visa tidigare resultat
+        if (choice == "2") {
+            game.ShowResult();
+        }
+
+        // Alternativ 4 - Avsluta program
+        if (choice == "3") {
+            break;
+        }
+
+        // Ogiltigt alternativ
+        if (choice == default)
+            Console.WriteLine("Ogiltigt val, försök igen!");
+            break;
+        }
+    }
+    }
 }
