8c0c3ebe00c8e8bd4071d76b5ddb1d0779acf0e1
Ängla Juliusson <angla02@icloud.com>
2025-11-03 10:50:36 +0100

    namespace -rockPaperScissorsGame

--- dt071g-projekt/Program.cs
+++ dt071g-projekt/Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+using System.Net.Http.Headers;
+using System.Text.Json;
+
+namespace rockPaperScissorsGame {
+
+
+}
