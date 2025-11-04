408bc3844a055c7acc243ced16c971c09858c90e
Ängla Juliusson <angla02@icloud.com>
2025-11-04 12:33:29 +0100

    Inkludera namespece i Game.cs

--- dt071g-projekt/Game.cs
+++ dt071g-projekt/Game.cs
@@ -1,3 +1,8 @@
+using System;
+using System.IO;
+
+namespace rockPaperScissorsGame {
+
 public class Game {
         // Statistik på resultat
 
@@ -199,3 +204,4 @@ public class Game {
             };
         }
     }
+}
