ed6647f31d905109ae131e7f9e0779d823fecaff
Ängla Juliusson <angla02@icloud.com>
2025-11-04 11:33:48 +0100

    Uppdarea usings

--- dt071g-projekt/Program.cs
+++ dt071g-projekt/Program.cs
@@ -1,7 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System;
-using System.Net.Http.Headers;
-using System.Text.Json;
+using System.IO;
 
 namespace rockPaperScissorsGame {
 
